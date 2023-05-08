1346e3416d216ec81edfe0202aa4ef624778c1ea
Joan Cortez Llorca <joluenti@gmail.com>
2023-05-08 15:50:09 +0200

    Tasca feta

--- src/com/company/DataType.java
+++ src/com/company/DataType.java
@@ -1,6 +1,13 @@
 package com.company;
 
+import java.util.*;
+
 public class DataType {
+
+    private static final Set<Integer> Date_Codes = new HashSet<>(Arrays.asList(702,1082,1083,1114,1184,1266,12403));
+
+    private static final Set<Integer> NUMERIC_CODES = new HashSet<>(Arrays.asList(
+            20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396));
 
     public static void main(String[] args) {
         System.out.println(DataType.gd(20));
@@ -8,35 +15,9 @@ public class DataType {
         System.out.println(DataType.gd(0));
     }
     public static String gd(int code){
-        if (
-                code == 702 ||
-                        code == 1082 ||
-                        code == 1083 ||
-                        code == 1114 ||
-                        code == 1184 ||
-                        code == 1266 ||
-                        code == 12403
-        ) {
+        if (Date_Codes.contains(code)) {
             return "DATE";
-        } else if (
-                code == 20 ||
-                        code == 21 ||
-                        code == 23 ||
-                        code == 24 ||
-                        code == 26 ||
-                        code == 700 ||
-                        code == 701 ||
-                        code == 790 ||
-                        code == 1700 ||
-                        code == 2202 ||
-                        code == 2203 ||
-                        code == 2204 ||
-                        code == 2205 ||
-                        code == 2206 ||
-                        code == 3734 ||
-                        code == 3769 ||
-                        code == 12396
-        ) {
+        } else if (NUMERIC_CODES.contains(code)) {
             return "NUMERIC";
         } else {
             return "STRING";
